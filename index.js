e56603582ebb6fd7c2cad3dfd720cf20b3d9a165
yussufhh <0793477632g@gmail.com>
2024-06-08 08:11:35 +0300

    final-commit-on-array-find-method

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+// function superbowlWin(record){
+// let records = record.find(function(element){
+// if(element.result==="W"){
+//   console.log(element.year) 
+// }
+// else{ return undefined}
+    
+// })
+
+// }
+function superbowlWin(record){
+  const winningRecord = record.find(play => play.result ==='W');
+  return winningRecord? winningRecord.year: undefined;
+}
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"}
+]
+
+
+
+superbowlWin(record)
+
